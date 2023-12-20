560deeb7164321ae6ec08d4143c39bec7d903148
wogus8285 <wogus8285@gmail.com>
2023-12-20 10:38:21 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <div className='heaedr'>
+        <h1>📘 노트</h1>
+        <button>쓰기</button>
+      </div>
+      <div className='list'>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+            eos autem repellendus, inventore assumenda quaerat voluptatibus
+            recusandae atque molestiae provident dolores doloribus perspiciatis
+            minima rerum quo reprehenderit odio modi dolore.
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+            eos autem repellendus, inventore assumenda quaerat voluptatibus
+            recusandae atque molestiae provident dolores doloribus perspiciatis
+            minima rerum quo reprehenderit odio modi dolore.
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+            eos autem repellendus, inventore assumenda quaerat voluptatibus
+            recusandae atque molestiae provident dolores doloribus perspiciatis
+            minima rerum quo reprehenderit odio modi dolore.
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+            eos autem repellendus, inventore assumenda quaerat voluptatibus
+            recusandae atque molestiae provident dolores doloribus perspiciatis
+            minima rerum quo reprehenderit odio modi dolore.
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+            eos autem repellendus, inventore assumenda quaerat voluptatibus
+            recusandae atque molestiae provident dolores doloribus perspiciatis
+            minima rerum quo reprehenderit odio modi dolore.
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목...</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+            eos autem repellendus, inventore assumenda quaerat voluptatibus
+            recusandae atque molestiae provident dolores doloribus perspiciatis
+            minima rerum quo reprehenderit odio modi dolore.
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+      </div>
+    </div>
+  )
+}
